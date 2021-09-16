8866741b5d0198fdfea6035c8c6563b220af17dc
Abhishek Dundagi <73735068+Abhishek-2505@users.noreply.github.com>
2021-09-16 12:38:32 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -25,10 +25,40 @@ df['avg_sal($)'] = ( df['min_sal($)'] + df['max_sal($)']) /2
 
 
 # company name
+df['Company'] = df.apply(lambda x: x['Company Name'] if x['Rating']<0 else x['Company Name'][:-3] , axis=1)
+
 
 # job state parse & location and hq same?
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+df['is_hq?'] = df.apply(lambda x: 1 if x['Location']==x['Headquarters'] else 0, axis=1)
+
 
 # age of company
+from datetime import date
+cur_date = date.today()
+df['company_age'] = df['Founded'].apply(lambda x: cur_date.year-x if x>0 else x)
+
 
 # job description parse (imp keywords like python etc)
+df['python'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+df['python'].value_counts()
 
+df['r_studio'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() else 0)
+df['r_studio'].value_counts()
+
+df['tableau'] = df['Job Description'].apply(lambda x: 1 if 'tableau' in x.lower() else 0)
+df['tableau'].value_counts()
+
+df['power_bi'] = df['Job Description'].apply(lambda x: 1 if 'power bi' in x.lower() else 0)
+df['power_bi'].value_counts()
+
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df['excel'].value_counts()
+
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df['spark'].value_counts()
+
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df['aws'].value_counts()
+
+df.to_csv('salary_data_cleaned.csv', index = False)
